ab2e5c5c53e264ec2da0094301b27677a36b91a6
HalukaMB <mail@maier-borst.de>
2023-09-13 11:07:09 +0200

    fiddling around with keys for project 3

--- Project3/script.js
+++ Project3/script.js
@@ -1,2 +1,7 @@
 let base =1
 document.getElementById("test").innerHTML="HELLO WORLD"
+console.log("hey")
+const keys_file=fetch("discogs.keys")
+.then((res) => res.text())
+.then((text) => console.log(text))
+
