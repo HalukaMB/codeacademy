09a8ac937a4ceec7610d5ca4ba4d36c32cba5c7d
HalukaMB <mail@maier-borst.de>
2023-11-23 15:07:45 +0100

    previous positions list displayed

--- Project6/trashOrNot/client/src/Components/MapElement.tsx
+++ Project6/trashOrNot/client/src/Components/MapElement.tsx
@@ -5,77 +5,76 @@ import getTrashLocations from '../hooks/getTrashLocations';
 
 const MapElement = () => {
 
-    const [trashLocation, setTrashLocation] =  useState<[number, number]>([0,0]);
-    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
-    const [previousPositions, setPreviousPositions] = useState<[number, number][]|null>(null);
+    const [previousPositions, setPreviousPositions] = useState<[number, number][] | null>(null);
+    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
 
-    const getPreviousLocations=()=>{
-    getTrashLocations().then(
-    (previousPoints:[])=>{
-        console.log(previousPoints)
-        setPreviousPositions(previousPoints)
-        })}
+    const getPreviousLocations = () => {
+        getTrashLocations().then(
+            (previousPoints: []) => {
+                console.log(previousPoints)
+                setPreviousPositions(previousPoints)
+            })
+    }
 
-    
     useEffect(() => {
         getPreviousLocations()
-      }, [])
-   
+    }, [])
+
+    const PreviousMarkers = () => {
+        if (previousPositions!=null){
+        return (
+            <>
+            {previousPositions.map((element) => (
+                <Marker key={element["lat"]}
+                position={[element["lat"],element["long"]]}
+                interactive={false}
+            />
+              ))
+            }
+            </>
+        )}
+    }
 
     const NewMarker = () => {
         const map = useMapEvents({
-            click(e) {     
-                console.log(e)                           
+            click(e) {
+                console.log(e)
                 setSelectedPosition([
                     e.latlng.lat,
                     e.latlng.lng
-                ]);                
-            },            
+                ]);
+            },
         })
         console.log(selectedPosition)
         return (
-            selectedPosition ? 
-                <Marker           
-                key={selectedPosition[0]}
-                position={selectedPosition}
-                interactive={false} 
+            selectedPosition ?
+                <Marker
+                    key={selectedPosition[0]}
+                    position={selectedPosition}
+                    interactive={false}
                 />
-            : null
-        )   
-        
-    }
-    const PreviousMarkers = () => {
-        return (
-            previousPositions ? 
-                <Marker           
-                key={previousPositions[0]["lat"]}
-                position={[previousPositions[0]["lat"],previousPositions[0]["long"]]}
-                interactive={false} 
-                />
-            : null
+                : null
         )
-        
-        
     }
 
     return (
-    <div><div>Put the pin on where you found the trash. Please be as accurate as possible.</div>
-        <div id="mapid">
-        <MapContainer center={[52.52, 13.41]} zoom={10}scrollWheelZoom={false}>
-        <NewMarker />
-        <PreviousMarkers />
+        <div><div>Put the pin on where you found the trash. Please be as accurate as possible.</div>
+            <div id="mapid">
+                <MapContainer center={[52.52, 13.41]} zoom={10} scrollWheelZoom={false}>
+                    <NewMarker />
+                    <PreviousMarkers />
 
 
-      <TileLayer
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      
-    </MapContainer>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    />
+
+                </MapContainer>
+            </div>
+
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default MapElement
