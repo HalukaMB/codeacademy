0d62658a0ddb72812a6ad950961807b3d37c0044
HalukaMB <mail@maier-borst.de>
2023-11-29 15:11:41 +0100

    navbar moves other parts oddly

--- Project6/trashOrNot/client/src/About.tsx
+++ Project6/trashOrNot/client/src/About.tsx
@@ -3,7 +3,10 @@ import { Navbar } from "./components/Navbar"
 const About = () => {
   return (
     <>
-    <Navbar></Navbar>
+      <div className="topSection">
+        <Navbar></Navbar>
+        <h1>Clean Berlin</h1>
+      </div>
     <div className="aboutPageContainer"><div className="intro">This is a project by Haluka Maier-Borst</div>
     <img src="src/assets/haluka_cropped.jpeg"></img>
     <div className='redirect'>If you like some what you see here, check out some of the code <a href="https://github.com/HalukaMB">here </a> 
