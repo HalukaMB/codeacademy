5e8896b2093b61b4221fe20fc914b53994cebd75
HalukaMB <mail@maier-borst.de>
2023-12-01 14:40:03 +0100

    toggle between register and login

--- Project6/trashOrNot/client/src/LoginOrRegister.tsx
+++ Project6/trashOrNot/client/src/LoginOrRegister.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { LoginLogout } from './components/LoginLogout'
 import { Register } from './components/Register'
 import { Navbar } from './components/Navbar'
@@ -6,20 +6,29 @@ import { TopSection } from './components/topSection'
 
 export const LoginOrRegister = () => {
 
-    const clickToggle = (event)=>{
-        console.log(event)
+    const [registerOrLogin, setRegisterOrLogin]=useState<string>("register")
+
+    const clickToggle = (event:React.MouseEvent<HTMLButtonElement, MouseEvent> )=>{
+        const clickedOnElement=(event.target as HTMLInputElement)
+        setRegisterOrLogin(clickedOnElement.value)
         
     }
 
     return (
         <>  <TopSection></TopSection>
+           
             <div className="loginSection">
+            <div className="toggleSection">
+                <button value="register" onClick={clickToggle}>Register</button>
+                <button value="login" onClick={clickToggle}>Login</button>
+            </div>
+            {(registerOrLogin=="register")?
                 <div className="toggleRegisterLogin">
                     <Register></Register>
-                </div>
+                </div>:
                 <div className="toggleRegisterLogin">
                     <LoginLogout></LoginLogout>
-                </div>
+                </div>}
             </div>
 
         </>
